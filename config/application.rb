dfb5e7f1562afe7794f3ba4e39156f104fbfaaa2
cijacob <jacobcindy@live.be>
2018-07-08 10:18:14 +0200

    Add mulitlanguages i18n

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ module ChallengeMe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :fr
+
+    I18n.available_locales = [:en, :nl, :fr]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  get '/:locale' => 'welcome#index'
   get 'welcome/index'
   get '/about', to: 'welcome#about'
   get '/career', to: 'welcome#career'
