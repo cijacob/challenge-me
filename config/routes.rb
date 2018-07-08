4e72cdcc734c66fa988e899a25b938d2c88b5804
cijacob <jacobcindy@live.be>
2018-07-08 11:16:05 +0200

    fix locale routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/:locale' => 'welcome#index'
+  
   get 'welcome/index'
   get '/about', to: 'welcome#about'
   get '/career', to: 'welcome#career'
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/:locale' => 'welcome#index'
 end
